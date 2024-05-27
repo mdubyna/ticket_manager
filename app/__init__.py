8693fef8ad80d0ea3b41a39220e08bcb16212bd0
Mykola <mykola22dubyna@gmail.com>
2024-05-27 17:06:48 +0300

    Add templates

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,7 @@ from flask_migrate import Migrate
 from config import Config
 
 
+
 db = SQLAlchemy()
 migrate = Migrate()
 
@@ -12,6 +13,10 @@ migrate = Migrate()
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
+
+    from app.models import Ticket
+    from app.routes.tickets_bp import tickets_bp
+    app.register_blueprint(tickets_bp, url_prefix="/")
 
     db.init_app(app)
     migrate.init_app(app, db)
--- run.py
+++ run.py
@@ -9,5 +9,6 @@ def hello():
     return "Hello World!"
 
 
+
 if __name__ == "__main__":
     app.run(debug=True)
